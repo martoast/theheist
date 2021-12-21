aeb9a3c3854dcdfc9dd4bd795eb931ea16733f88
Alejandro Martos <alexmartos96@gmail.com>
2021-12-20 22:50:10 -0800

    about-us

---
+++ pages/about-us.vue
@@ -0,0 +1,111 @@
+<template>
+  <div style="background-color: #000 !important">
+    <b-container class="p-3">
+      <b-row align-h="center" class="text-center">
+        <b-col>
+          <h1 class="title text-white">What Is The Heist?</h1>
+        </b-col>
+      </b-row>
+      <div class="about text-white">
+        <p>The Heist is an innovative NFT Collection</p>
+        <p>
+          The Heist Group consists of 3 women who find themselves at a
+          crossroad. A drunk and impulsive night turned into risking everything
+          they have and moving across the country in an attempt to change their
+          lives. Little did they know how much their lives would actually
+          change. Between terrible decisions and awful money management they
+          find themselves having to choose between options.. Continuing down the
+          straight and narrow path, or getting into a life of crime. The choice
+          is made for them when they accidentally (but almost by choice) kill a
+          friend’s A$$hole boyfriend. After this horrific incident- there is no
+          turning back.
+        </p>
+        <p>
+          They start to plot and scheme their way into New York, robbing several
+          gas stations and eventually a bank on the way.
+        </p>
+        <p>
+          The papers... ugh... the papers! They never seem to get it right. Some
+          say that they wear masks because they are hiding something, others say
+          they wear wigs and a bare face. OH! The horror- for who would believe
+          3 gorgeous 20 year old women would cause such a ruckus.
+        </p>
+        <p>
+          They breeze through the small towns unnoticed and uncaptured. Leaving
+          nothing behind but a trail of empty cash registers and male bank
+          tellers who piss their pants.
+        </p>
+        <p>
+          Once they arrive in New York they lay low, they try to get normal jobs
+          and do normal things. But let's be honest- once you get a taste of the
+          pie, you can never say no. And their stomachs are hungry, more hungry
+          for bigger and better things, FASTER!- I mean, isn’t that what New
+          York does to everyone?
+        </p>
+        <p>
+          They hit Atlantic City to hide all of the money they've collected...
+          This is where The Heist Group was born.
+        </p>
+        <p>
+          A committee of all women that broke free from the rules of society,
+          from poverty, and the strict laws of their Government.
+        </p>
+        <p>
+          They created a union of 10,000 ruthless female trained escape artists,
+          specialized in Heist Missions against this system. Spectators seem to
+          only catch but a glimpse of the crimes they commit. Sly, prestigious
+          but executing their plans precisely.
+        </p>
+        <p>
+          Some say that thousands of them are all dressed in different attire
+          when seen in person, others think it is the same 3 girls doing it all.
+          Legend has it, by the time the local authorities are to be contacted
+          for help, a cloud of dust is seen out of the building. Some have ideas
+          of what they look like, and others say it is all a myth. Not only are
+          these women professional bank robbers, jewelry thieves, and hijackers
+          on a block chain, they plan to expand inside of the METAVERSE It is
+          believed they run the largest Crime Union on the Western Hemisphere
+          called the Heist Group and they are looking for expansion!
+        </p>
+        <p>But like they say in the Papers- “It may all just be a myth”</p>
+        <p>xo,</p>
+        <p>THG</p>
+        <img style="height: 36px" src="/Mel-Signature.svg" />
+      </div>
+    </b-container>
+  </div>
+</template>
+<style scoped>
+@font-face {
+  font-family: "Abys";
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~assets/fonts/abys-regular.otf") format("truetype");
+}
+
+@font-face {
+  font-family: "komik";
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~assets/fonts/komik.ttf") format("truetype");
+}
+
+@font-face {
+  font-family: "Champange";
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("~assets/fonts/Champagne.ttf") format("truetype");
+}
+
+.about {
+  font-family: "Champange";
+  font-size: 22px;
+}
+.title {
+  font-family: "komik";
+  font-size: 32px;
+}
+</style>
