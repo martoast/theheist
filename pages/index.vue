0c5c5432d8d8ad7dfc3ad8892de988100332a447
Alejandro Martos <alexmartos96@gmail.com>
2021-12-28 11:06:20 -0800

    coming soon

--- pages/index.vue
+++ pages/index.vue
@@ -56,11 +56,14 @@
                 </b-col>
               </b-row>
 
-              <b-button
+              <!-- <b-button
                 href="//mint.theheist.world"
                 pill
                 class="enter-btn border-0"
                 >PRESALE LIVE !</b-button
+              > -->
+              <b-button pill class="enter-btn border-0"
+                >PRESALE COMING SOON</b-button
               >
             </div>
           </div>
