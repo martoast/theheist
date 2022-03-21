82f0db81cdbc2a33e2e8037efe268f9a552c0080
Alejandro Martos <alexmartos96@gmail.com>
2022-03-20 20:24:48 -0700

    text change

--- components/MeetTeam.vue
+++ components/MeetTeam.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <b-card-group class="text-white" deck>
+    <b-card-group class="text-white" columns>
       <b-card
         style="background-color: #000 !important"
-        img-src="/Mel.png"
+        img-src="/Mel.jpg"
         img-alt="Image"
         img-top
       >
@@ -11,10 +11,13 @@
           >MGMS aka "GOD"</b-card-title
         >
         <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          Fashion Designer by day, illustrator by night. Melanie Mercado has
-          been juggling the fashion industry for over 11 yrs while creating a
-          universe of illustrations in her free time (and cutting into her sleep
-          time- but then again… God never sleeps).
+          Fashion Designer by day, illustrator by night.
+          <span style="font-weight: bold; font-size: 22px"
+            >Melanie Mercado</span
+          >
+          has been juggling the fashion industry for over 11 yrs while creating
+          a universe of illustrations in her free time (and cutting into her
+          sleep time- but then again… God never sleeps).
         </b-card-text>
       </b-card>
 
@@ -28,10 +31,13 @@
           >Mastermind</b-card-title
         >
         <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          Enrique Marrero began his career in the world of computing by creating
-          custom private servers of existing gaming platforms. He is always
-          networking with new persons, and companies, of interest which is a
-          task he does not take lightly as the team leader of The Heist.
+          <span style="font-weight: bold; font-size: 22px"
+            >Enrique Marrero</span
+          >
+          began his career in the world of computing by creating custom private
+          servers of existing gaming platforms. He is always networking with new
+          persons, and companies, of interest which is a task he does not take
+          lightly as the team leader of The Heist.
         </b-card-text>
       </b-card>
 
@@ -45,10 +51,12 @@
           >The Hacker</b-card-title
         >
         <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          <span>Alejandro Martos</span> is a Software Engineer with a love for
-          technology and it's potential to disrupt paradigms. In his
-          professional work he has demonstrable experience developing and
-          deploying complex web applications.
+          <span style="font-weight: bold; font-size: 22px"
+            >Alejandro Martos</span
+          >
+          is a Mexico based Software Engineer with years of industry experience
+          developing complex web applications. He has a passion and love for
+          technology and it's potential to disrupt paradigms.
         </b-card-text>
       </b-card>
 
@@ -59,13 +67,51 @@
         img-top
       >
         <b-card-title style="font-family: 'Market'; font-size: 34px"
-          >WingWoman</b-card-title
+          >Social Butterfly</b-card-title
         >
         <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          Ashley is Melanie's bestfriend and stusty WingWoman. She is a Realtor
-          by day who has taken the role as Marketing Director by the horns-
-          Learning a new skill, joining The Heist World with the rest of the
-          team.
+          Ashely is a multifaceted human, with her experience running through
+          multiple channels, spaces y industries. (Sales, Real estate,
+          Merchandising, Styling to name a few) And now- She has hijacked The
+          Heist Twitter- don’t forget to say Hi when you stop by!
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        style="background-color: #000 !important"
+        img-src="/Kevin.jpg"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-title style="font-family: 'Market'; font-size: 34px"
+          >The Architect</b-card-title
+        >
+        <b-card-text style="font-family: 'Champange'; font-size: 18px">
+          <span style="font-weight: bold; font-size: 22px"
+            >Kevin Adrian Di Nardo</span
+          >
+          is a Miami based architectural designer and 3D artist with over 10
+          years of design experience. Kevin holds a Master’s Degree in
+          Architecture and is the recipient of the 2019 Miami AIA Merit Award of
+          Excellence.
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        style="background-color: #000 !important"
+        img-src="/Jazz.png"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-title style="font-family: 'Market'; font-size: 34px"
+          >The Investor</b-card-title
+        >
+        <b-card-text style="font-family: 'Champange'; font-size: 18px">
+          We will take this space to thank him, for believing, trusting and
+          allowing us to move forward with our vision. For being there when we
+          called for help, and for being open to this new venture. We are
+          blessed to have someone who has not interfered with our creative
+          process and to be able to call him a friend.
         </b-card-text>
       </b-card>
     </b-card-group>
