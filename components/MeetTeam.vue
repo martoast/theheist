9aecae0f423b82d6b358cbd529db1c8c657841b9
Alejandro Martos <alexmartos96@gmail.com>
2022-03-20 21:23:16 -0700

    error

--- components/MeetTeam.vue
+++ components/MeetTeam.vue
@@ -34,10 +34,10 @@
           <span style="font-weight: bold; font-size: 22px"
             >Kevin Adrian Di Nardo</span
           >
-          Kevin Adrian Di Nardo is a Miami based architectural designer and 3D
-          artist. As the Architect of the team, he brings over 10 years of
-          design experience to The Heist and is dedicated to manifesting a
-          metaverse for its community to populate, explore and enjoy.
+          is a Miami based architectural designer and 3D artist. As the
+          Architect of the team, he brings over 10 years of design experience to
+          The Heist and is dedicated to manifesting a metaverse for its
+          community to populate, explore and enjoy.
         </b-card-text>
       </b-card>
 
