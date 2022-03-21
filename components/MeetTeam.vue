0ded3f8e96d25625a730be27b60f6d69fa70d795
Alejandro Martos <alexmartos96@gmail.com>
2022-03-20 20:30:51 -0700

    team arrangement

--- components/MeetTeam.vue
+++ components/MeetTeam.vue
@@ -23,6 +23,26 @@
 
       <b-card
         style="background-color: #000 !important"
+        img-src="/Kevin.jpg"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-title style="font-family: 'Market'; font-size: 34px"
+          >The Architect</b-card-title
+        >
+        <b-card-text style="font-family: 'Champange'; font-size: 18px">
+          <span style="font-weight: bold; font-size: 22px"
+            >Kevin Adrian Di Nardo</span
+          >
+          is a Miami based architectural designer and 3D artist with over 10
+          years of design experience. Kevin holds a Master’s Degree in
+          Architecture and is the recipient of the 2019 Miami AIA Merit Award of
+          Excellence.
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        style="background-color: #000 !important"
         img-src="/kike.png"
         img-alt="Image"
         img-top
@@ -38,6 +58,24 @@
           servers of existing gaming platforms. He is always networking with new
           persons, and companies, of interest which is a task he does not take
           lightly as the team leader of The Heist.
+        </b-card-text>
+      </b-card>
+
+      <b-card
+        style="background-color: #000 !important"
+        img-src="/Jazz.png"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-title style="font-family: 'Market'; font-size: 34px"
+          >The Investor</b-card-title
+        >
+        <b-card-text style="font-family: 'Champange'; font-size: 18px">
+          We will take this space to thank him, for believing, trusting and
+          allowing us to move forward with our vision. For being there when we
+          called for help, and for being open to this new venture. We are
+          blessed to have someone who has not interfered with our creative
+          process and to be able to call him a friend.
         </b-card-text>
       </b-card>
 
@@ -74,44 +112,6 @@
           multiple channels, spaces y industries. (Sales, Real estate,
           Merchandising, Styling to name a few) And now- She has hijacked The
           Heist Twitter- don’t forget to say Hi when you stop by!
-        </b-card-text>
-      </b-card>
-
-      <b-card
-        style="background-color: #000 !important"
-        img-src="/Kevin.jpg"
-        img-alt="Image"
-        img-top
-      >
-        <b-card-title style="font-family: 'Market'; font-size: 34px"
-          >The Architect</b-card-title
-        >
-        <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          <span style="font-weight: bold; font-size: 22px"
-            >Kevin Adrian Di Nardo</span
-          >
-          is a Miami based architectural designer and 3D artist with over 10
-          years of design experience. Kevin holds a Master’s Degree in
-          Architecture and is the recipient of the 2019 Miami AIA Merit Award of
-          Excellence.
-        </b-card-text>
-      </b-card>
-
-      <b-card
-        style="background-color: #000 !important"
-        img-src="/Jazz.png"
-        img-alt="Image"
-        img-top
-      >
-        <b-card-title style="font-family: 'Market'; font-size: 34px"
-          >The Investor</b-card-title
-        >
-        <b-card-text style="font-family: 'Champange'; font-size: 18px">
-          We will take this space to thank him, for believing, trusting and
-          allowing us to move forward with our vision. For being there when we
-          called for help, and for being open to this new venture. We are
-          blessed to have someone who has not interfered with our creative
-          process and to be able to call him a friend.
         </b-card-text>
       </b-card>
     </b-card-group>
