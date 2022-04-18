571213317658f16089ed1dc64ac1cd32d2f66340
Alejandro Martos <alexmartos96@gmail.com>
2022-04-17 20:22:38 -0700

    road map phase 1

--- pages/index.vue
+++ pages/index.vue
@@ -311,6 +311,51 @@ A committee of all women that broke free from the rules of society, from poverty
          </b-row>
       </b-container>
 
+      <b-container class="p-3">
+      <b-row align-h="center" class="text-center">
+        <b-col>
+          <h1 class="text-white" style="font-family: 'Market'; font-size: 42px;">THE HEIST ROADMAP</h1>
+        </b-col>
+      </b-row>
+      <h1 class="title text-white text-center" style="font-family: 'Market'; font-size: 42px;">PHASE 01</h1>
+      <div class="about text-white pt-2 pt-md-4">
+        <h1 style="font-family: 'Market'; font-size: 42px;">Q1 2022</h1>
+        <div style="font-family: 'Champange';">
+           <p>● Project Launch ✅</p>
+        <p>● Begin organic community development</p>
+        <p>● Early Community Pre-Sale ✅</p>
+        <p>● New Web platform launch ✅</p>
+        <p>● Recruitment of professionals to join administrative team ✅</p>
+        <p>● Team Reveal ✅</p>
+        <p>● Rewards for OG and Early Holders dispersed ✅</p>
+        <p>● Periodic AMA’s begin, schedules to be announced in Discord ✅</p>
+        <p>
+          ● 0.5 ETH Private Sale for Legendary 1/1 Giveaway – end of March ✅
+        </p>
+        <h1 style="font-family: 'Market'; font-size: 42px;">Q2 2022</h1>
+        <p>● Begin professional marketing campaigns ✅</p>
+        <p>● List project on NFT Calendar ✅</p>
+        <p>● Get listed on Rarity.tools</p>
+        <p>● 3D model First Metaverse Heist Lounge for Discord Community</p>
+        <p>● .05 ETH Public Sale of Genesis Collection begins!</p>
+        <p>● Merch design development/consultation with manufacturer</p>
+        <p>● First scenes of the The Heist Comic shall be released</p>
+        <h1 style="font-family: 'Market'; font-size: 42px;">Q3 2022</h1>
+        <p>● Continuation of rewards for active community members</p>
+        <p>
+          ● Launch first Heist Metaverse Lounge room to host Community events
+        </p>
+        <p>● Release further promotional content to grow community</p>
+        <p>● Launch first merch bundle to holders only</p>
+        <p>● Close public sale</p>
+        <p>● Generate next installment of Heist Lounge</p>
+        <p>● Release new scenes of The Heist Comic saga</p>
+
+        </div>
+        
+      </div>
+    </b-container>
+
       <b-container class="py-5">
          <h2 class="text-center text-white py-3" style="font-family: 'Market'; font-size: 42px;">Meet the Team</h2>
          <meet-team></meet-team>
@@ -334,6 +379,8 @@ A committee of all women that broke free from the rules of society, from poverty
          </b-col>
          
       </b-row>
+
+      
 
 
 
--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -6,47 +6,38 @@
           <h1 class="title text-white">THE HEIST ROADMAP</h1>
         </b-col>
       </b-row>
+      <h1 class="title text-white">PHASE 01:</h1>
       <div class="about text-white pt-2 pt-md-4">
-        <p class="font-weight-bold">
-          The Heist is a Community Driven project That will be officially
-          launched at the beginning of 2022
-        </p>
         <h1 class="title">Q1 2022</h1>
-        <h1>Announcement for our OG holders</h1>
+        <p>● Project Launch ✅</p>
+        <p>● Begin organic community development</p>
+        <p>● Early Community Pre-Sale ✅</p>
+        <p>● New Web platform launch ✅</p>
+        <p>● Recruitment of professionals to join administrative team ✅</p>
+        <p>● Team Reveal ✅</p>
+        <p>● Rewards for OG and Early Holders dispersed ✅</p>
+        <p>● Periodic AMA’s begin, schedules to be announced in Discord ✅</p>
         <p>
-          • We will announce the Rewards for our first ever nft holders that
-          minted on our first presale
-        </p>
-        <h1>Team Reveal</h1>
-        <p>• Reveal all team member identities</p>
-        <h1>The Heist Agents</h1>
-        <p>
-          • THE HEIST AGENTS ( secondary and exclusive nft collection only
-          available for our first 3,000 Holders )
-        </p>
-        <h1>Storyline Breakdown/Reveal</h1>
-        <p>
-          • We will reveal the storyline and start offering the community to
-          choose the direction of The Heist
+          ● 0.5 ETH Private Sale for Legendary 1/1 Giveaway – end of March ✅
         </p>
         <h1 class="title">Q2 2022</h1>
-        <h1>Web Platform Announcement</h1>
-        <p>Announcing our Desktop/Mobile web platform and release dates</p>
-        <h1>The Heist Multiverse</h1>
-        <p>• Announcing or next projects and next steps for our Multiverse</p>
+        <p>● Begin professional marketing campaigns ✅</p>
+        <p>● List project on NFT Calendar ✅</p>
+        <p>● Get listed on Rarity.tools</p>
+        <p>● 3D model First Metaverse Heist Lounge for Discord Community</p>
+        <p>● .05 ETH Public Sale of Genesis Collection begins!</p>
+        <p>● Merch design development/consultation with manufacturer</p>
+        <p>● First scenes of the The Heist Comic shall be released</p>
         <h1 class="title">Q3 2022</h1>
-        <h1>Acquire Virtual Land for our MetaVerse community</h1>
-        <p>• We will acquire Land on the MetaVerse for our Holders</p>
-        <h1>Reveal our Second Collection</h1>
+        <p>● Continuation of rewards for active community members</p>
         <p>
-          The Heist Season 2 will be announced along with our second NFT
-          collection
+          ● Launch first Heist Metaverse Lounge room to host Community events
         </p>
-        <h1 class="title">Q4 2022</h1>
-        <h1>Play to Earn Games Reveal</h1>
-        <p>• We will reveal a teaser trailer for our Play to Earn Game</p>
-        <h1>Release of second Roadmap</h1>
-        <p>• The Second Roadmap will be Revealed</p>
+        <p>● Release further promotional content to grow community</p>
+        <p>● Launch first merch bundle to holders only</p>
+        <p>● Close public sale</p>
+        <p>● Generate next installment of Heist Lounge</p>
+        <p>● Release new scenes of The Heist Comic saga</p>
       </div>
     </b-container>
   </div>
